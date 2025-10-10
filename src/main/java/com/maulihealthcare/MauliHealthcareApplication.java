a654579a25d0e2088168a75fe318b26c99574fbd
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2025-10-10 20:59:40 +0530

    initial commit

---
+++ src/main/java/com/maulihealthcare/MauliHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.maulihealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauliHealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauliHealthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maulihealthcare/MauliHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maulihealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauliHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
