2926e34bc162288ef2b2e41faf311d0f1d633e44
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2025-10-14 00:44:43 +0530

    fixed: add Annotations to Proscription class

--- src/main/java/com/maulihealthcare/entity/Prescription.java
+++ src/main/java/com/maulihealthcare/entity/Prescription.java
@@ -1,13 +1,32 @@
 package com.maulihealthcare.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "prescriptions")
+@Data
 public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "appointment_id", nullable = false)
     private Appointment appointment;
+
+    @Column(columnDefinition = "TEXT")
     private String diagnosis;
+
+    @Column(columnDefinition = "TEXT")
     private String medicines;
-    private String dosege;
+
+    @Column(columnDefinition = "TEXT")
+    private String dosage;
+
+    @Column(columnDefinition = "TEXT")
     private String notes;
     private LocalDate followUpDate;
 
