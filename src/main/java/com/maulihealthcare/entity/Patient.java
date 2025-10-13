53fe233f2e87f9b558485b6868785ceaa5e06bcf
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2025-10-13 16:32:54 +0530

    feat: add attributes in Patient class

--- src/main/java/com/maulihealthcare/entity/Patient.java
+++ src/main/java/com/maulihealthcare/entity/Patient.java
@@ -1,4 +1,23 @@
 package com.maulihealthcare.entity;
 
+import org.springframework.boot.autoconfigure.security.saml2.Saml2RelyingPartyAutoConfiguration;
+
 public class Patient {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private String password;
+
+    private String role ="PATIENT";
+    private Integer age;
+    private String gender;
+    private String bloodgroup;
+
 }
