c41bf4836e26c2c78484017d35b05536b2d70513
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2025-10-15 21:21:55 +0530

    feat: add DoctorDTO

---
+++ src/main/java/com/maulihealthcare/dto/DoctorDTO.java
@@ -0,0 +1,16 @@
+package com.maulihealthcare.dto;
+
+public class DoctorDTO {
+    private Long id;
+    private String name;
+    private String specialization;
+    private String phone;
+    private String email;
+    private String password;
+    private String availableDays;
+    private String role;
+    private String gender;
+    private Integer experienceYears;
+    private String department;
+
+}
